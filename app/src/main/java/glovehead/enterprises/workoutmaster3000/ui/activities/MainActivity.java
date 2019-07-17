a992ac44161a0210ace46eb16ccae4cfbb03576b
Mike <thechimaira@gmail.com>
2019-07-17 17:32:56 +0100

    Removed janky main activity recycler view

--- app/src/main/java/glovehead/enterprises/workoutmaster3000/ui/activities/MainActivity.java
+++ app/src/main/java/glovehead/enterprises/workoutmaster3000/ui/activities/MainActivity.java
@@ -1,12 +1,15 @@
 package glovehead.enterprises.workoutmaster3000.ui.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,26 +22,19 @@ import glovehead.enterprises.workoutmaster3000.viewmodels.MainActivityViewModel;
 public class MainActivity extends AppCompatActivity {
 
     private MainActivityViewModel viewModel;
+    private Button startWorkoutBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RecyclerView recyclerView = findViewById(R.id.main_activity_recycler_view);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setHasFixedSize(true);
-        final WorkoutPlanSelectorAdapter planSelectorAdapter = new WorkoutPlanSelectorAdapter();
-        recyclerView.setAdapter(planSelectorAdapter);
-
         viewModel = ViewModelProviders.of(this).get(MainActivityViewModel.class);
         viewModel.getActiveWorkoutSessionPlan().observe(this, new Observer<WorkoutSessionPlan>() {
             @Override
             public void onChanged(WorkoutSessionPlan workoutSessionPlan) {
                 // update recycler view
-                List<WorkoutSessionPlan> activePlanList = new ArrayList<>();
-                activePlanList.add(workoutSessionPlan);
-                planSelectorAdapter.setWorkoutSessionPlans(activePlanList);
+
             }
         });
     }
